06f9d62a883d1673b91788f88bf122f51cc8a73a
Jelle de Jong <jelle@infoprojects.nl>
2011-08-15 19:47:23 +0200

    Changed sponsor

--- vvz49/footer.php
+++ vvz49/footer.php
@@ -6,7 +6,7 @@
             <a href="http://www.neoprint.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/Neoprint.jpg" width="167" height="50" /></a>
             <a href="http://www.sport2000.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/sport2000.jpg" width="49" height="50" /></a>
             <a href="http://www.eetcafecentraal.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/Centraal.jpg" width="144" height="50" /></a>
-            <a href="http://www.teakcity.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/wooncity.jpg" width="152" height="50" /></a>
+            <a href="http://www.boshuizenenko.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/boshuizenenko.jpg" width="135" height="50" /></a>
             <a href="http://www.autosmeeing.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/smeeing.jpg" width="101" height="50" /></a>
           </div>
         </div>
