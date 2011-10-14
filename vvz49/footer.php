fc62f582a1820c8ce52d9b0790fe1cf7865640c6
Jelle de Jong <jelle@infoprojects.nl>
2011-10-14 13:43:33 +0200

    Banner en nieuwe logo voor Smeeing is toegevoegd

--- vvz49/footer.php
+++ vvz49/footer.php
@@ -7,7 +7,7 @@
             <a href="http://www.sport2000.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/sport2000.jpg" width="49" height="50" /></a>
             <a href="http://www.eetcafecentraal.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/Centraal.jpg" width="144" height="50" /></a>
             <a href="http://www.boshuizenenko.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/boshuizenenko.jpg" width="135" height="50" /></a>
-            <a href="http://www.autosmeeing.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/smeeing.jpg" width="101" height="50" /></a>
+            <a href="http://www.autosmeeing.nl/" target="_blank"><img src="/wp-content/themes/vvz49/images/autosmeeing_logo.jpg" width="211" height="50" /></a>
           </div>
         </div>
       </div>
