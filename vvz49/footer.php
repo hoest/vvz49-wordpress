7626ab3e2f72f54fa6101d7589a5075a6698ff51
Jelle de Jong <jelle@infoprojects.nl>
2012-07-18 08:57:27 +0200

    Aanpassing sponsoren

--- vvz49/footer.php
+++ vvz49/footer.php
@@ -4,7 +4,7 @@
             <p>Sponsoren:</p>
             <a href="http://www.reprobaarn.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/Reprobaarn.jpg" width="38" height="50" /></a>
             <a href="http://www.neoprint.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/Neoprint.jpg" width="167" height="50" /></a>
-            <a href="http://www.sport2000.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/sport2000.jpg" width="49" height="50" /></a>
+            <a href="http://www.abog-vastgoed.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/abog-vastgoed.jpg" width="146" height="50" /></a>
             <a href="http://www.eetcafecentraal.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/Centraal.jpg" width="144" height="50" /></a>
             <a href="http://www.boshuizenenko.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/boshuizenenko.jpg" width="135" height="50" /></a>
             <a href="http://www.autosmeeing.nl/" target="_blank"><img alt="" src="/wp-content/themes/vvz49/images/autosmeeing_logo.jpg" width="211" height="50" /></a>
