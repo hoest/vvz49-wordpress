be6269f89a21d131e2dfe5ac6af503eae41a7d3a
Jelle de Jong <jelle@hoest.nl>
2013-09-17 21:51:06 +0200

    Forms functionaliteiten toegevoegd

--- vvz49/header.php
+++ vvz49/header.php
@@ -30,12 +30,16 @@
       }
     ?></title>
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <link rel="stylesheet" type="text/css" href="/wp-content/plugins/ninja-forms/css/ninja-forms-display.css" />
+    <link rel="stylesheet" type="text/css" href="/wp-content/plugins/ninja-forms/css/qtip.css" />
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
     <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
     <?php wp_head(); ?>
     <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s laatste berichten', 'vvz49' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
     <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s laatste reacties', 'vvz49' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="http://cufon.shoqolate.com/js/cufon-yui.js" type="text/javascript"></script>
     <script src="/wp-content/themes/vvz49/js/calibri.cufonfonts.js" type="text/javascript"></script>
     <script type="text/javascript">
